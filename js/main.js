54fbe3c31a9d068bd51c34f16f1f83affb3f29cd
Astro Aguilar <theastrocodingproject@gmail.com>
2024-02-29 00:37:44 -0700

    added const variables

---
+++ js/main.js
@@ -0,0 +1,17 @@
+//below will be all my const, and let variables
+const root = document.documentElement
+const gameContainer = document.getElementById('gameGridContainer')
+const gameGridContainer = document.getElementById('gameGridShadow')
+const gameGridOverlay = document.getElementById('gameGridShadow')
+const slider = document.getElementById('slider')
+const bubble = document.getElementById('bubble')
+const clearButton = document.getElementById('clearButton')
+const eraserButton = document.getElementById('eraserButton')
+const blackButton = document.getElementById('blackButton')
+const rainbowButton = document.getElementById('rainbowButton')
+const greyscaleButton = document.getElementById('greyscaleButton')
+const selectColorButton = document.getElementById('selectColorButton')
+const selectColorButtonContainer = document.getElementById('selectColorButtonContainer')
+let colorMode = 'blackButton';
+
+//below will be my functions
